b4e4461cc89dd8a6bd51c8b3b078768fb0fd3297
Hugo Shaka <hugo.hervieux@free.fr>
2017-04-29 12:30:09 +0200

    Random seed initialized, more comprehensive vars.

--- weightsLSTM.cc
+++ weightsLSTM.cc
@@ -12,36 +12,38 @@ WeightsLSTM::WeightsLSTM(int input_size, int output_size) {
     this->input_size = input_size;
     this->output_size = output_size;
 
+// We make sure the random seed is initialized
+srand(time(NULL));
 // We initialize random weights
-    this->weight_in_forget_gate = 0.1 * Eigen::MatrixXd::Random(
+    this->W_in_forget_gate = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->input_size);
 
-    this->weight_in_input_gate = 0.1 * Eigen::MatrixXd::Random(
+    this->W_in_input_gate = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->input_size);
 
-    this->weight_in_input_block = 0.1 * Eigen::MatrixXd::Random(
+    this->W_in_input_block = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->input_size);
 
-    this->weight_in_output_gate = 0.1 * Eigen::MatrixXd::Random(
+    this->W_in_output_gate = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->input_size);
 
-    this->weight_st_forget_gate = 0.1 * Eigen::MatrixXd::Random(
+    this->W_prev_forget_gate = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->output_size);
 
-    this->weight_st_input_gate = 0.1 * Eigen::MatrixXd::Random(
+    this->W_prev_input_gate = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->output_size);
 
-    this->weight_st_input_block = 0.1 * Eigen::MatrixXd::Random(
+    this->W_prev_input_block = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->output_size);
 
-    this->weight_st_output_gate = 0.1 * Eigen::MatrixXd::Random(
+    this->W_prev_output_gate = 0.1 * Eigen::MatrixXd::Random(
         this->output_size,
         this->output_size);
 
@@ -59,35 +61,35 @@ WeightsLSTM::WeightsLSTM(int input_size, int output_size) {
 
 // We initialize a null gradient
 
-    this->delta_weight_in_forget_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_in_forget_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_in_input_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_in_input_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_in_input_block = Eigen::MatrixXd::Zero(
+    this->delta_W_in_input_block = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_in_output_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_in_output_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_st_forget_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_forget_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 
-    this->delta_weight_st_input_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_input_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 
-    this->delta_weight_st_input_block = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_input_block = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 
-    this->delta_weight_st_output_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_output_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 /*
@@ -99,37 +101,37 @@ WeightsLSTM::WeightsLSTM(int input_size, int output_size) {
 }
 void WeightsLSTM::apply_gradient(double lambda) {
 // We apply the weight variations
-    this->weight_in_forget_gate =
-        this->weight_in_forget_gate
-        - lambda * this->delta_weight_in_forget_gate;
+    this->W_in_forget_gate =
+        this->W_in_forget_gate
+        + lambda * this->delta_W_in_forget_gate;
 
-    this->weight_in_input_gate =
-        this->weight_in_input_gate
-        - lambda * this->delta_weight_in_input_gate;
+    this->W_in_input_gate =
+        this->W_in_input_gate
+        + lambda * this->delta_W_in_input_gate;
 
-    this->weight_in_input_block =
-        this->weight_in_input_block
-        - lambda * this->delta_weight_in_input_block;
+    this->W_in_input_block =
+        this->W_in_input_block
+        + lambda * this->delta_W_in_input_block;
 
-    this->weight_in_output_gate =
-        this->weight_in_output_gate
-        - lambda * this->delta_weight_in_output_gate;
+    this->W_in_output_gate =
+        this->W_in_output_gate
+        + lambda * this->delta_W_in_output_gate;
 
-    this->weight_st_forget_gate =
-        this->weight_st_forget_gate
-        - lambda * this->delta_weight_st_forget_gate;
+    this->W_prev_forget_gate =
+        this->W_prev_forget_gate
+        + lambda * this->delta_W_prev_forget_gate;
 
-    this->weight_st_input_gate =
-        this->weight_st_input_gate
-        - lambda * this->delta_weight_st_input_gate;
+    this->W_prev_input_gate =
+        this->W_prev_input_gate
+        + lambda * this->delta_W_prev_input_gate;
 
-    this->weight_st_input_block =
-        this->weight_st_input_block
-        - lambda * this->delta_weight_st_input_block;
+    this->W_prev_input_block =
+        this->W_prev_input_block
+        + lambda * this->delta_W_prev_input_block;
 
-    this->weight_st_output_gate =
-        this->weight_st_output_gate
-        - lambda * this->delta_weight_st_output_gate;
+    this->W_prev_output_gate =
+        this->W_prev_output_gate
+        + lambda * this->delta_W_prev_output_gate;
 
 /*
     this->bias_forget_gate -= lambda * this->delta_bias_forget_gate;
@@ -139,35 +141,35 @@ void WeightsLSTM::apply_gradient(double lambda) {
 */
 
 // We set a null gradient
-    this->delta_weight_in_forget_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_in_forget_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_in_input_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_in_input_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_in_input_block = Eigen::MatrixXd::Zero(
+    this->delta_W_in_input_block = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_in_output_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_in_output_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->input_size);
 
-    this->delta_weight_st_forget_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_forget_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 
-    this->delta_weight_st_input_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_input_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 
-    this->delta_weight_st_input_block = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_input_block = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 
-    this->delta_weight_st_output_gate = Eigen::MatrixXd::Zero(
+    this->delta_W_prev_output_gate = Eigen::MatrixXd::Zero(
         this->output_size,
         this->output_size);
 /*
